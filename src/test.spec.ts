38c271c367fca9ec8fe95e2699c0d76c363a4cab
Jedd Niñonuevo <jed@jasetech.ca>
2024-10-31 17:27:46 +0800

    Refactor Tests

--- src/test.spec.ts
+++ src/test.spec.ts
@@ -1,10 +1,10 @@
 import formValidator from "$lib/formValidator";
 import { test, expect } from "vitest";
 
-test("Test Form Validator Function", () => {
-	// Check if form validator returns no error when provided with correct values
+test("Check if form validator returns no error when provided with correct values", () => {
 	expect(formValidator({ name: "Robert", age: 10 })).toStrictEqual({});
+});
 
-	// Check if form validator is a valid type
+test("Check if form validator is a valid type", () => {
 	expect(formValidator({ name: "", age: 0 })).toBeTypeOf("object");
 });
